29a9750850a31fb7577c08da320c1d862dc857b2
Roman <kordukroman@gmail.com>
2018-05-20 21:28:50 +0300

    added LinqToObjectTask, File reading and writing

---
+++ Classes/Client.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace LinqHomework
+{
+	public partial class Program
+	{
+		private static readonly StreamWriter WriterForClient =
+			new StreamWriter("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\objectResults.txt");
+
+		private class Client
+		{
+			private string _year;
+			private string _mounth;
+
+			public Client(string id = " ", string y = " ", string m = " ", string o = " ")
+			{
+				Id = id;
+				Year = y;
+				Mounth = m;
+				Occupation = o;
+			}
+
+			public string Id { get; set; }
+
+			public string Year
+			{
+				get => _year;
+				set
+				{
+					if (Convert.ToInt32(_year) > 0)
+					{
+						_year = value;
+					}
+				}
+			}
+
+			public string Mounth
+			{
+				get => _mounth;
+				set
+				{
+					if (Convert.ToInt32(_mounth) > 0)
+					{
+						_mounth = value;
+					}
+				}
+			}
+
+			public string Occupation { get; set; }
+		}
+
+		private static Client ReadFromFile(TextReader streamReader)
+		{
+			var dataForClient = new Client
+			{
+				Id = streamReader.ReadLine(),
+				Year = streamReader.ReadLine(),
+				Mounth = streamReader.ReadLine(),
+				Occupation = streamReader.ReadLine()
+			};
+			return dataForClient;
+		}
+
+		private static List<Client> ReadClassFromFile()
+		{
+			var clients = new List<Client>();
+			var streamReader = new StreamReader("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\objectData.txt");
+			var amount = Convert.ToInt32(streamReader.ReadLine());
+
+			for (var i = 0; i < amount; i++)
+			{
+				clients.Add(ReadFromFile(streamReader));
+			}
+
+			return clients;
+		}
+
+		private static void TasksForClient(IReadOnlyCollection<Client> clients)
+		{
+			var minOcupation = clients.Min(i => i.Occupation);
+			WriterForClient.WriteLine("Minimal occupation in hours:");
+			WriterForClient.WriteLine(minOcupation);
+
+			var maxOcupation = clients.Max(occup => occup.Occupation);
+			WriterForClient.WriteLine("Maximal occupation in hours:");
+			WriterForClient.WriteLine(maxOcupation);
+
+			WriterForClient.WriteLine("Maximal occupation year:");
+			var groupedByYears = clients.GroupBy(year => year.Year);
+
+			IEnumerable<IGrouping<string, Client>> sorted = groupedByYears
+				.OrderByDescending(group => group.Sum(occup => Convert.ToInt32(occup.Occupation)))
+				.ThenBy(item => item.Min(year => year.Year));
+
+			WriterForClient.WriteLine(sorted.First().Key);
+			WriterForClient.WriteLine("Sum of occupations for all years: ");
+
+			var groupedById = clients.GroupBy(id => id.Id);
+			var durations = new SortedDictionary<string, string>();
+
+			foreach (var group in groupedById)
+			{
+				var sum = group.Sum(ocur => Convert.ToInt32(ocur.Occupation));
+				durations.Add(Convert.ToString(sum), group.Key);
+			}
+
+			foreach (var group in durations)
+			{
+				WriterForClient.WriteLine("Occupation : {0} ,Id: {1}", group.Key, group.Value);
+			}
+
+			WriterForClient.WriteLine("Count of amount: ");
+			var groupedByCode2 = clients.GroupBy(id => id.Id);
+			var months = new SortedDictionary<int, int>();
+
+			foreach (var group in groupedByCode2)
+			{
+				var count = group.Count();
+				months.Add(Convert.ToInt32(group.Key), count);
+			}
+
+			foreach (var group in months)
+			{
+				WriterForClient.WriteLine("Id : {0} ,Occupation: {1}", group.Key, group.Value);
+			}
+		}
+	}
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace LinqHomework
+{
+	partial class Program
+	{
+		private static readonly StreamWriter Writer =
+			new StreamWriter("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\result.txt");
+
+		private static void Main(string[] args)
+		{
+			try
+			{
+				LinqBegin16();
+				Console.WriteLine("---");
+				LinqBegin17();
+				Console.WriteLine("---");
+				LinqBegin18();
+				Console.WriteLine("---");
+				LinqBegin19();
+				Console.WriteLine("---");
+				LinqBegin20();
+				Console.WriteLine("---");
+				LinqBegin44();
+				Console.WriteLine("---");
+				LinqBegin45();
+				Console.WriteLine("---");
+
+				var dataForClients = ReadClassFromFile();
+				TasksForClient(dataForClients);
+				Writer.Close();
+				WriterForClient.Close();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Error! Can't open the file");
+				throw;
+			}
+		}
+
+		private static void LinqBegin16()
+		{
+			var numbers = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\tasks(16-18,20).txt");
+			var positive = numbers.Where(num => Convert.ToInt32(num) > 0);
+			var enumerable = positive.ToList();
+
+			foreach (var i in enumerable)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 16");
+			foreach (var item in enumerable)
+			{
+				Writer.Write($"{item} ");
+			}
+
+			Writer.WriteLine();
+		}
+
+		private static void LinqBegin17()
+		{
+			var numbers = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\tasks(16-18,20).txt");
+			var oddNumbers = numbers.Where(num => Convert.ToInt32(num) % 2 != 0).Distinct();
+			var enumerable = oddNumbers.ToList();
+			foreach (var i in enumerable)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 17");
+			foreach (var item in enumerable)
+			{
+				Writer.Write($"{item} ");
+			}
+
+			Writer.WriteLine();
+		}
+
+		private static void LinqBegin18()
+		{
+			var numbers = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\tasks(16-18,20).txt");
+			var newNumbers = numbers.Where(num => Convert.ToInt32(num) > 9 && Convert.ToInt32(num) < 100)
+				.OrderBy(num => Convert.ToInt32(num));
+
+			var enumerable = newNumbers.ToList();
+			foreach (var i in enumerable)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 18");
+			foreach (var item in enumerable)
+			{
+				Writer.Write($"{item} ");
+			}
+
+			Writer.WriteLine();
+		}
+
+		private static void LinqBegin19()
+		{
+			var words = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\task19.txt");
+			var sortedWords = words.OrderBy(word => word.Length).ThenByDescending(word => word);
+
+			foreach (var i in sortedWords)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 19");
+			foreach (var item in sortedWords)
+			{
+				Writer.Write($"{item} ");
+			}
+
+			Writer.WriteLine();
+		}
+
+		private static void LinqBegin20()
+		{
+			var numbers = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\tasks(16-18,20).txt");
+			const int d = 172;
+			var newNumbers = numbers.SkipWhile(num => Convert.ToInt32(num) < d)
+				.Where(num => Convert.ToInt32(num) % 2 != 0 && Convert.ToInt32(num) > 0).Reverse();
+
+			var enumerable = newNumbers.ToList();
+			foreach (var i in enumerable)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 20");
+			foreach (var item in enumerable)
+			{
+				Writer.Write($"{item} ");
+			}
+
+			Writer.WriteLine();
+		}
+
+		private static void LinqBegin44()
+		{
+			var numbersA = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\task44A.txt");
+			var numbersB = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\task44B.txt");
+
+			const int k1 = 60;
+			const int k2 = 144;
+
+			var result = numbersA.Where(num => Convert.ToInt32(num) > k1)
+				.Concat(numbersB.Where(num => Convert.ToInt32(num) < k2))
+				.OrderBy(num => Convert.ToInt32(num)).DefaultIfEmpty();
+
+			foreach (var i in result)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 44");
+			foreach (var item in result)
+			{
+				Writer.Write($"{item} ");
+			}
+
+			Writer.WriteLine();
+		}
+
+		private static void LinqBegin45()
+		{
+			var wordsA = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\task45A.txt");
+			var wordsB = File.ReadAllLines("C:\\Users\\Roman\\RiderProjects\\LinqHomework\\LinqHomework\\task45B.txt");
+
+			const int l1 = 3;
+			const int l2 = 5;
+
+			var result = wordsA.Where(word => word.Length == l1).Concat(wordsB.Where(word => word.Length == l2))
+				.OrderByDescending(word => word)
+				.DefaultIfEmpty();
+
+			var enumerable = result.ToList();
+			foreach (var i in enumerable)
+			{
+				Console.WriteLine(i);
+			}
+
+			Writer.WriteLine("Task 45");
+			foreach (var item in enumerable)
+			{
+				Writer.Write($"{item} ");
+			}
+		}
+	}
+}
